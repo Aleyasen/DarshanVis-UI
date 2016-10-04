03a47b8462d2e782bbadda9e9fa0dd69f7b2f140
Amirhossein Aleyasin <amir.8829@gmail.com>
2016-10-04 10:38:17 -0500

    add chart 12 (not complete), add highchart js

--- darshan-ui/client/templates/charts/chart1.js
+++ darshan-ui/client/templates/charts/chart1.js
@@ -20,6 +20,39 @@ Template.chart1.rendered = function () {
   };
   sendreq(filtervals, chart_id, function (data) {
     console.log(data);
-    
+    var options = charts[chart_id];
+    var series = chart_series[chart_id];
+    // console.log("SERIES:");
+    // console.log(series);
+    var queryResult = data;
+    var opts_series = [];
+    for (var i = 0; i < series.length; i++) {
+      if (!series[i]["not-in-chart"]) {
+        var attr = series[i]["attribute"];
+        var qr = queryResult[attr];
+        if (qr != null) {
+          series[i]["data"] = [];
+          for (var j = 0; j < qr.length; j++) {
+            var num = Number(qr[j]);
+            if (num != 0) {
+              series[i]["data"].push([j, num]);
+            }
+          }
+          opts_series.push(series[i]);
+        }
+      }
+    }
+    // console.log("OPTS SERIES");
+    console.log(opts_series);
+
+    options.series = opts_series;
+
+    // options.renderTo = ".chart_container[data_vis=" + data_vis + "]";
+    // var chart = new Highcharts.Chart(options);
+    var selector = ".chart_container[data_vis='" + data_vis + "']";
+    console.log(selector);
+    console.log($(selector));
+    Highcharts.chart(selector, options);
+
   });
 };
