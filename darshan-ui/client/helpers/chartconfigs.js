cf64346275127b27cf48f9c579fe3fb4eed85653
Amirhossein Aleyasin <amir.8829@gmail.com>
2016-10-06 10:22:04 -0500

    update breakdown chart

--- darshan-ui/client/helpers/chartconfigs.js
+++ darshan-ui/client/helpers/chartconfigs.js
@@ -1,3 +1,24 @@
+Highcharts.setOptions({
+  chart: {
+    zoomType: "xy",
+    width: 900,
+    height: 500
+    // backgroundColor: {
+    //   linearGradient: [0, 0, 500, 500],
+    //   stops: [
+    //     [0, 'rgb(255, 255, 255)'],
+    //     [1, 'rgb(240, 240, 255)']
+    //   ]
+    // },
+    // borderWidth: 2,
+    // plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+    // plotShadow: true,
+    // plotBorderWidth: 1
+  }
+});
+
+
+
 charts = {
   "17": {
     "title": {
